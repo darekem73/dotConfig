41b419eb0e3ef535d56719819ea6f5e78e706d60
Dariusz Mastalerz <darek.mastalerz@gmail.com>
2022-09-22 23:12:13 +0200

    add void-lightdm

--- autostart.sh
+++ autostart.sh
@@ -9,14 +9,14 @@ conky -d -c ~/.conkyrc-stat
 #xfce4-power-manager &
 #unclutter -idle 1 -root &
 #mopidy &
-compton &
-picom &
+#compton &
+#picom &
 setxkbmap -option caps:escape
 xset r rate 200 40
 slstatus &
-synclient TapButton1=1 TapButton2=3 TapButton3=2
-synclient RightButtonAreaLeft=0 RightButtonAreaTop=0
-xautolock -time 10 -locker slock
+#synclient TapButton1=1 TapButton2=3 TapButton3=2
+#synclient RightButtonAreaLeft=0 RightButtonAreaTop=0
+#xautolock -time 10 -locker slock
 
 # if [[ `xrandr | egrep connected | grep -v dis | wc -l` -eq 2 ]]; then 
 # 	monitor extend
