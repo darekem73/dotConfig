7788ef1f119aecea4d7bdbcf6ae620176e6753f7
Dariusz Mastalerz <darek.mastalerz@gmail.com>
2022-09-30 16:42:23 +0200

    mod: atc.rb

--- usr_bin/atc.rb
+++ usr_bin/atc.rb
@@ -4,7 +4,7 @@ require 'yaml'
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: #{opts.program_name} { --config CONF --colors COLORS }"
+  opts.banner = "Usage: #{opts.program_name} { --config CONF [ --colors COLORS ] [ --font-size FS ] }"
   $BANNER = opts.banner
   opts.on("--config FILE_NAME", "alacritty config") do |f|
     options[:config] = f
@@ -12,22 +12,32 @@ OptionParser.new do |opts|
   opts.on("--colors FILE_NAME", "colors definition file") do |f|
     options[:colors] = f
   end
+  opts.on("--font-size FS", "font size") do |f|
+    options[:fs] = f
+  end
   opts.on("-h","--help","Help") do
     puts opts
     exit
   end
 end.parse!
 
-if not options[:config] or not options[:colors]
+if not options[:config]
   puts "Use -h"
   exit
 end
 
 config=YAML.load(File.read(options[:config]))
-colors=YAML.load(File.read(options[:colors]))
+colors=YAML.load(File.read(options[:colors])) if options[:colors]
+fs = options[:fs]
 
-if config and colors
+if colors
   puts "Applying: #{options[:colors]}"
   config["colors"]=colors["colors"]
-  File.write(options[:config],config.to_yaml)
 end
+
+if fs
+  puts "Applying font size: #{fs}"
+  config["font"]["size"] = fs.to_i
+end
+
+File.write(options[:config],config.to_yaml)
